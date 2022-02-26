66c5eb9ffb72555047e651106154486947ce1569
AirBurn <rosan1233456@gmail.com>
2022-02-26 03:10:59 +0300

    stream works lmao

--- src/main/java/ru/mail/polis/baidiyarosan/InMemoryDao.java
+++ src/main/java/ru/mail/polis/baidiyarosan/InMemoryDao.java
@@ -5,30 +5,35 @@ import ru.mail.polis.Dao;
 
 import java.nio.ByteBuffer;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.NavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.function.Function;
 
 public class InMemoryDao implements Dao<ByteBuffer, BaseEntry<ByteBuffer>> {
 
-    private final NavigableMap<ByteBuffer, BaseEntry<ByteBuffer>> collection = new ConcurrentSkipListMap<>();
+    private final NavigableMap<ByteBuffer, ByteBuffer> collection = new ConcurrentSkipListMap<>();
+
+    private final Function<Map.Entry<ByteBuffer, ByteBuffer>, BaseEntry<ByteBuffer>> mapper =
+            (e) -> new BaseEntry(e.getKey(), e.getValue());
 
     @Override
     public Iterator<BaseEntry<ByteBuffer>> get(ByteBuffer from, ByteBuffer to) {
 
         if (from == null && to == null) {
-            return collection.values().iterator();
+            return collection.entrySet().stream().map(mapper).iterator();
         }
 
         return collection.subMap(
                 from == null ? collection.firstKey() : from, true,
                 to == null ? collection.lastKey() : to, to == null
-        ).values().iterator();
+        ).entrySet().stream().map(mapper).iterator();
 
     }
 
     @Override
     public void upsert(BaseEntry<ByteBuffer> entry) {
-        collection.put(entry.key(), entry);
+        collection.put(entry.key(), entry.value());
     }
 
 }
